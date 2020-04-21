0500d6e8a4b7a42ad02ce51c1e4793bdca292b9e
Sernicalm <nico@neuronasistemas.com>
2020-04-21 07:45:37 +0200

    Agregamos y modificamos modulos

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from "@angular/core";
+
+//RUTAS
+import { PAGES_ROUTES } from './pages.routes';
+
+
+//MODULOS
+import { SharedModule } from '../shared/shared.module';
+
+//PAGINAS
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+import { PagesComponent } from './pages.component';
+
+
+
+@NgModule({
+    declarations:[
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component,
+        PagesComponent
+    ],
+    exports:[
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component,
+    ],
+    imports:[
+        SharedModule,
+        PAGES_ROUTES
+    ]
+})
+
+
+export class PagesModule{}
